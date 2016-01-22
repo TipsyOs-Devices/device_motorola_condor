10ba4d04b577f17dea5775736a4a067b79f13eda
Vatsal <koradiavatsal@gmail.com>
2016-01-22 15:51:40 +0530

    Use 4.9
    Change-Id: Iba8415c4d16b92baddfb24940dee543018bc93d9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,14 @@ LOCAL_PATH := device/motorola/condor
 TARGET_KERNEL_CONFIG := cm_condor_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8610
 
+#Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# Rom Toolchain
+TARGET_GCC_VERSION_EXP := 4.9-uber
+
+
 # Storage & partiiton
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10444800
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10526720
